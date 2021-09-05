d86cfd608bccff197f269d794a08d25967ead88c
Avramov Kirill <kirillka3003@gmail.com>
2021-09-05 10:38:44 +0300

    feat: complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,50 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.isLower = null;
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        if (this.isLower == null) {
+            this.result = Math.ceil((this.max + this.min) / 2);
+            return this.result;
+        }
+        if (this.isLower == true) {
+            this.max = this.result;
+            this.result = Math.ceil((this.min + this.max) / 2);
+            return this.result;
+        }
+        if (this.isLower == false) {
+            this.min = this.result;
+            this.result = Math.ceil((this.max + this.min) / 2);
+            return this.result;
+        }
     }
 
     lower() {
-
+        this.isLower = true;
     }
 
     greater() {
-
+        this.isLower = false;
     }
 }
 
+const game = new GuessingGame();
+game.setRange(0, 7340)
+let result = game.guess();
+        game.lower();
+        result = game.guess();
+        game.lower();
+        result = game.guess();
+        game.lower();
+        result = game.guess();
+        game.greater();
 module.exports = GuessingGame;
